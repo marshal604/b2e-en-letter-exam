69c98f46a9f041600c1c56f680128ebf5df551fb
Lerdor <marshal604@gmail.com>
2019-06-18 20:43:53 +0800

    init login api and db query.

---
+++ doc/auth.md
@@ -0,0 +1,12 @@
+## AUTH
+
+### Login API
+
+### DB
+#### user_info
+```
+CREATE TABLE "user_info" (
+    id   CHAR(50) PRIMARY KEY NOT NULL,
+    body jsonb
+);
+```
---
+++ server/src/db/auth/auth.db.ts
@@ -0,0 +1,42 @@
+import { BaseDB } from '@db/base.db';
+import { LoginOAuthRequest, LoginRequest, UserInfo } from '@models/auth/auth.model';
+
+export class AuthDB extends BaseDB {
+  tableName = 'user_info';
+
+  constructor() {
+    super();
+  }
+
+  createUser(req: LoginOAuthRequest): Promise<void> {
+    const body = JSON.stringify(req);
+    const sql = `INSERT INTO ${this.tableName} VALUES('${req.userId}', '${body}');`;
+    return new Promise((resolve, reject) => {
+      this.psql
+        .none(sql)
+        .then(() => resolve())
+        .catch(error => reject(error));
+    });
+  }
+
+  findUser(req: LoginOAuthRequest | LoginRequest): Promise<UserInfo> {
+    let sql = `SELECT * FROM ${this.tableName} WHERE 1 = 1 `;
+    const loginOAuthReq: LoginOAuthRequest = req as LoginOAuthRequest;
+    const loginReq: LoginRequest = req as LoginRequest;
+
+    if (loginOAuthReq.userId) {
+      sql += `AND body->>'userId' = '${loginOAuthReq.userId}'`;
+    } else if (loginReq.account) {
+      sql += `AND body->>'account' = '${loginReq.account}' AND body->>'password' = '${
+        loginReq.password
+      }'`;
+    }
+
+    return new Promise((reslove, reject) => {
+      this.psql
+        .oneOrNone(sql)
+        .then(res => reslove(res))
+        .catch(err => reject(err));
+    });
+  }
+}
